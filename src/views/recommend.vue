0a46d9bf3dd5092d21e84139101b633fff209816
yunumu <1416900346@qq.com>
2023-02-26 22:31:44 +0800

    推荐页面

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,28 +1,64 @@
 <template>
-  <div class="recommend">
-    <div class="slider-wrapper">
-      <div class="slider-content">
-        <Slider v-if="sliders.length" :sliders="sliders" />
+  <div class="recommend" v-loading:[loadingText]="loading">
+    <scroll class="recommend-content">
+      <div>
+        <div class="slider-wrapper">
+          <div class="slider-content">
+            <slider v-if="sliders.length" :sliders="sliders"></slider>
+          </div>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title" v-show="!loading">热门歌单推荐</h1>
+          <ul>
+            <li
+                v-for="item in albums"
+                class="item"
+                :key="item.id">
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.pic" :src="item.pic"/>
+              </div>
+              <div class="text">
+                <h2 class="name">
+                  {{ item.username }}
+                </h2>
+                <p class="title">
+                  {{ item.title }}
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script>
-import { getRecommend } from '@/service/recommend'
+import { getRecommend } from '../service/recommend'
 import Slider from '@/components/base/slider/slider'
-
+import Scroll from '@/components/base/scroll/scroll'
 export default {
-  name: "recommend",
-  components: { Slider },
-  data() {
+  name: 'recommend',
+  components: {
+    Slider,
+    Scroll
+  },
+  data () {
     return {
-      sliders: []
+      sliders: [],
+      albums: [],
+      loadingText: '加载中'
     }
   },
-  async mounted() {
-    const res = await getRecommend();
-    this.sliders = res.sliders;
+  computed: {
+    loading () {
+      return !this.sliders.length && !this.albums.length
+    }
+  },
+  async created () {
+    const result = await getRecommend()
+    this.sliders = result.sliders
+    this.albums = result.albums
   }
 }
 </script>
@@ -34,18 +70,15 @@ export default {
   top: 88px;
   bottom: 0;
   overflow: scroll;
-
   .recommend-content {
     height: 100%;
     overflow: hidden;
-
     .slider-wrapper {
       position: relative;
       width: 100%;
       height: 0;
       padding-top: 40%;
       overflow: hidden;
-
       .slider-content {
         position: absolute;
         left: 0;
@@ -54,7 +87,6 @@ export default {
         height: 100%;
       }
     }
-
     .recommend-list {
       .list-title {
         height: 65px;
@@ -63,19 +95,16 @@ export default {
         font-size: $font-size-medium;
         color: $color-theme;
       }
-
       .item {
         display: flex;
         box-sizing: border-box;
         align-items: center;
         padding: 0 20px 20px 20px;
-
         .icon {
           flex: 0 0 60px;
           width: 60px;
           padding-right: 20px;
         }
-
         .text {
           display: flex;
           flex-direction: column;
@@ -85,16 +114,15 @@ export default {
           overflow: hidden;
           font-size: $font-size-medium;
         }
-
         .name {
           margin-bottom: 10px;
           color: $color-text;
         }
-
         .title {
           color: $color-text-d;
         }
       }
     }
   }
-}</style>
+}
+</style>
