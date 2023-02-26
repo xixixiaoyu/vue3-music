e81e8a9120dd3dacb7d381672486cfc90cdf9599
yunumu <1416900346@qq.com>
2023-02-26 22:36:31 +0800

    歌手列表基础渲染

--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,17 +1,34 @@
 <template>
-  <div>
-    singer
+  <div>Singer</div>
+  <div class="singer" v-loading="!singers.length">
+    <index-list :data="singers"></index-list>
   </div>
 </template>
 
 <script>
+import { getSingerList } from '../service/singer'
+import IndexList from '@/components/base/index-list/index-list'
+
 export default {
-  setup() {
-
-
-    return {}
+  name: 'singer',
+  components: { IndexList },
+  data() {
+    return {
+      singers: []
+    }
+  },
+  async created() {
+    const result = await getSingerList()
+    this.singers = result.singers
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
+</style>
