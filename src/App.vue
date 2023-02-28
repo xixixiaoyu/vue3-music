246f7e6a9bed4f64b9a46161b55450cbcb14be28
yunumu <1416900346@qq.com>
2023-03-01 00:30:32 +0800

    新增播放器组件，点击列表歌曲播放

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <m-header></m-header>
     <tab></tab>
     <router-view></router-view>
+    <player></player>
   </div>
 </template>
 
 <script>
 import Header from '@/components/header/header.vue'
 import Tab from '@/components/tab/tab.vue'
+import Player from '@/components/player/player.vue'
 
 export default {
   name: "App",
   components: {
     MHeader: Header,
-    Tab
+    Tab,
+    Player
   }
 }
 </script>
