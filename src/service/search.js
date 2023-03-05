e355f1a085d814a85cd8677c3013d08c69c0f7ed
yunumu <1416900346@qq.com>
2023-03-05 21:03:02 +0800

    搜索功能

--- src/service/search.js
+++ src/service/search.js
@@ -1,10 +1,10 @@
 import { get } from './base'
 
-export function getHotKeys () {
+export function getHotKeys() {
   return get('/api/getHotKeys')
 }
 
-export function search (query, page, showSinger) {
+export function search(query, page, showSinger) {
   return get('/api/search', {
     query,
     page,
--- src/views/search.vue
+++ src/views/search.vue
@@ -1,17 +1,112 @@
 <template>
-  <div>
-    search
+  <div class="search">
+    <div class="search-input-wrapper">
+      <search-input v-model="query"></search-input>
+    </div>
+    <div class="search-content" v-show="!query">
+      <div class="hot-keys">
+        <h1 class="title">热门搜索</h1>
+        <ul>
+          <li
+            class="item"
+            v-for="item in hotKeys"
+            :key="item.id"
+            @click="addQuery(item.key)"
+          >
+            <span>{{ item.key }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="search-result">
+      <suggest :query="query"></suggest>
+    </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import SearchInput from '@/components/search/search-input'
+import Suggest from '@/components/search/suggest'
+import { getHotKeys } from '@/service/search'
+
 export default {
+  name: 'search',
+  components: { SearchInput, Suggest },
   setup() {
-
-
-    return {}
+    // 搜索参数
+    const query = ref('')
+    // 热门搜索
+    const hotKeys = ref([{ key: '郑淳元' }, { key: '单依纯' }, { key: '黄霄云' }])
+    // 获取热门搜索数据
+    // getHotKeys().then(result => {
+    //   hotKeys.value = result.hotKeys
+    // })
+    // 查询热门搜索内容
+    function addQuery(hotWord) {
+      query.value = hotWord
+    }
+    return { query, hotKeys, addQuery }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.search {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  .search-input-wrapper {
+    margin: 20px;
+  }
+  .search-content {
+    flex: 1;
+    overflow: hidden;
+    .hot-keys {
+      margin: 0 20px 20px 20px;
+      .title {
+        margin-bottom: 20px;
+        font-size: $font-size-medium;
+        color: $color-text-l;
+      }
+      .item {
+        display: inline-block;
+        padding: 5px 10px;
+        margin: 0 20px 10px 0;
+        border-radius: 6px;
+        background: $color-highlight-background;
+        font-size: $font-size-medium;
+        color: $color-text-d;
+      }
+    }
+    .search-history {
+      position: relative;
+      margin: 0 20px;
+      .title {
+        display: flex;
+        align-items: center;
+        height: 40px;
+        font-size: $font-size-medium;
+        color: $color-text-l;
+        .text {
+          flex: 1;
+        }
+        .clear {
+          @include extend-click();
+          .icon-clear {
+            font-size: $font-size-medium;
+            color: $color-text-d;
+          }
+        }
+      }
+    }
+  }
+  .search-result {
+    flex: 1;
+    overflow: hidden;
+  }
+}
+</style>
